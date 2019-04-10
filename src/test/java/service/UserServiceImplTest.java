52ae52f0c98b716ef5425986d7c474343df18a88
Cosmin Bresug <marnicos2006@yahoo.com>
2019-04-10 21:50:26 +0100

    setup test dependencies

---
+++ src/test/java/service/UserServiceImplTest.java
@@ -0,0 +1,24 @@
+package service;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+class UserServiceImplTest {
+
+    @Before
+    void setUp() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void addUser() {
+    }
+}
