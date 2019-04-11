eb155d45fe1d6c02d24aed4acf744f1001148cc0
Cosmin Bresug <marnicos2006@yahoo.com>
2019-04-12 00:06:47 +0100

    Added integration tests

---
+++ src/test/java/web/UserControllerTest.java
@@ -0,0 +1,78 @@
+package web;
+
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import service.UserService;
+
+import static com.jayway.restassured.RestAssured.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Before
+    public void setUp(){
+        RestAssured.baseURI="http://localhost";
+        RestAssured.port=8080;
+    }
+
+    @Test
+    public void getUsersShouldReturn200IfSuccessful() {
+        given()
+                .when().get("/user/get/1")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void getAllUsersShouldReturn200IfSuccessful() {
+        given()
+                .when().get("/user/get")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void addUserShouldReturn200IfSuccessful() {
+        JSONObject user = new JSONObject()
+                .put("username", "Username")
+                .put("firstName", "First name")
+                .put("lastName", "Last name");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(user.toString())
+                .when().post("/user/add")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void addUserShouldReturn403IfFieldsMissing() {
+        JSONObject user = new JSONObject()
+                .put("username", "Username")
+                .put("firstName", "First name");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(user.toString())
+                .when().post("/user/add")
+                .then()
+                .statusCode(403);
+    }
+}
