9da8c52aafcc4e79333b737f084bb6180db71606
Cosmin Bresug <cosmin.bresug@genieai.co>
2019-04-11 13:29:57 +0100

    Initial set of tests

--- src/test/java/service/UserServiceImplTest.java
+++ src/test/java/service/UserServiceImplTest.java
@@ -1,24 +1,64 @@
 package service;
 
 
+import model.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import repository.UserRepository;
 
-class UserServiceImplTest {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceImplTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    List<User> users;
+    User user;
 
     @Before
-    void setUp() {
+    public void setUp() {
+        users = Arrays.asList(
+                new User(1L, "Username", "First", "Last"),
+                new User(2L, "Username", "First", "Last"));
+        user = new User(1L, "Username", "First", "Last");
     }
 
     @Test
-    void getAllUsers() {
+    public void getAllUsersShouldReturnUserList() {
+        when(userRepository.getAllUsers()).thenReturn(users);
+        List<User> userList = userService.getAllUsers();
+
+        assertEquals(userList.size(), 2);
+        assertEquals(userList.get(0).getId(), (Long) 1L);
+        assertEquals(userList.get(1).getId(), (Long) 2L);
     }
 
     @Test
-    void getUser() {
+    public void getUserShouldReturnUser() {
+        when(userRepository.getUser(1L)).thenReturn(Collections.singletonList(user));
+        List<User> userList = userService.getUser(1L);
+
+        assertEquals(userList.size(), 1);
+        assertEquals(userList.get(0).getId(), (Long) 1L);
     }
 
     @Test
-    void addUser() {
+    public void addUser() {
+        userService.addUser(user);
+        verify(userRepository, times(1)).addUser(user);
     }
 }
